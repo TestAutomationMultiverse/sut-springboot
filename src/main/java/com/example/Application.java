d92d01d82103d98fca5956c8137eb2bf83010bf2
Mahmoud-Eltohamy <mahmoud.mohammed.elhady@gmail.com>
2024-09-14 04:19:08 +0300

    init

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    /**
+     * Starts the Spring Boot application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
